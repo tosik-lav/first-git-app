6c1901cd32ede756249f716ab7af34e9d8e6502b
Antonina Chinkova <tosik8lav@gmail.com>
2021-04-04 12:42:20 +0300

    Initial commit

---
+++ src/com/google/Main.java
@@ -0,0 +1,8 @@
+package com.google;
+//https://git-scm.com/book/ru/v2/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%A7%D1%82%D0%BE-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-Git%3F
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
